a550800e206e6d3ab1706592eb1c4236713da997
Loan <loanhoangphuong2212@gmail.com>
2019-04-15 11:55:44 +0700

    Trang thông tin cá nhân đã chỉnh sửa

--- resources/views/userindex.blade.php
+++ resources/views/userindex.blade.php
@@ -9,13 +9,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="stylesheet" type="text/css" href="userindex.css">
+  <link rel="stylesheet" type="text/css" href="css/userindex.css">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <title>Home</title>
-
+ <base href="{{asset('')}}">
   <!-- Custom fonts for this template-->
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
