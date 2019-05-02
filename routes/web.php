2ca0acb8820c5f97809872fb3872e54267483fde
ndq3004 <ndq3004@gmail.com>
2019-05-02 20:50:20 +0700

    add multi check

--- routes/web.php
+++ routes/web.php
@@ -81,13 +81,13 @@ Route::get('getrole','UserController@GetRole');
 // Route::get('adminLogin', 'AdminController@getLoginAdmin');
 // Route::post('adminLogin','AdminController@postLoginAdmin');
 
-Route::get('index',[
-	'as'=>'index','uses'=>'AdminController@index'
-],'middleware'=>'login');
-Route::get('courseAdmin',
-	['as'=>'courseAdmin','uses'=>'AdminController@course'
-], 'middleware'=>'login');
+// Route::get('index',
+// 	['as'=>'index','uses'=>'AdminController@index'],
+// 	 'middleware'=>'login');
+// Route::get('courseAdmin',
+// 	['as'=>'courseAdmin','uses'=>'AdminController@course'
+// ], 'middleware'=>'login');
 
-Route::get("userAdmin", ['as'=>'userAdmin','uses'=>'AdminController@users'
-], 'middleware'=>'login');
-Route::get('lienket', 'AdminController@user1');
+// Route::get("userAdmin", ['as'=>'userAdmin','uses'=>'AdminController@users'
+// ], 'middleware'=>'login');
+// Route::get('lienket', 'AdminController@user1');
